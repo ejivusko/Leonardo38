7583ea28bf95741c95a1eec9a30942b40f1afba0
ejivusko <ejivusko@gmail.com>
2022-08-13 01:34:55 +0300

    a1

--- assets/Folder/skin.js
+++ assets/Folder/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.11/18043
 // Filename: holderSet01.ggsk
-// Generated 2022-08-13T01:16:03
+// Generated 2022-08-13T01:33:42
 
 function pano2vrSkin(player,base) {
 	player.addVariable('var_margin', 2, false);
